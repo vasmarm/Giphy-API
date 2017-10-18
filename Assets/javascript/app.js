82dd5fb06fe7ac9ae3bbefb11205abf72eb94c96
Vashisth Sharma <vinnie@Vashisths-MacBook-Pro.local>
2017-10-17 23:10:11 -0500

    GiphyApi 4th  commit

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -3,21 +3,13 @@
 var topics = ["Bat", "Bike", "Cliff", "Baseball", "Grey", "Pillow", "Blue", "Window", "Keyboard"];
 
 
-//calling the function getGifInfo 
-/*$("#addanimal").on("click", function() {
-  console.log("hi");
-  getURL();
-   
-});*/
-
-// Function for displaying movie data
+// Function for displaying buttons
 function renderButtons(){
 
-  // Deleting the buttons prior to adding new animals
-  // (this is necessary otherwise you will have repeat buttons)
+  // Deleting the buttons prior to adding new topics
   $("#buttons-view").empty();
 
-  // Looping through the array of animals
+  // Looping through the array of topics
   for (var i = 0; i < topics.length; i++) {
 
     // Then dynamicaly generating buttons for each animal in the array
@@ -38,60 +30,66 @@ function renderButtons(){
   }
 }
 
+//Calling renderButtons at the starting so that buttons could be displayed which are present in the topics array
 renderButtons();
 
+//Function for getting the URL and calling the getGifInfo function
 function getURL(){
 
-
-   //capturing the data-name 
+   //Capturing the data-name in a variable
    var searchAnimal = $(this).attr("data-name");
 
-   //url for the API along with the API Key which is declared globally
-   var url = "https://api.giphy.com/v1/gifs/search?q=" + searchAnimal + "&rating=g&limit=2&api_key=TolAYTQXtHkC4X2gJaA7gy8NjY2XS0o4";
+   //Url for the API which passes searchAnimal along with the API Key 
+   var url = "https://api.giphy.com/v1/gifs/search?q=" + searchAnimal + "&rating=g&limit=10&api_key=TolAYTQXtHkC4X2gJaA7gy8NjY2XS0o4";
 
-   //calling the function getGifInfo to fetch the API object while passing the URL into it
+   //Calling the function getGifInfo to fetch the API object while passing the URL into it
    getGifInfo(url);
 }
 
+//Function getGifInfo
 function getGifInfo(urlLink){
 
-    $("#results").empty();
+  //results area is cleared at the starting so that previous results are not displayed
+  $("#results").empty();
 
-    //AJAX call using the urlLink passed in the function
+  //AJAX call using the urlLink passed in the function
   $.ajax({
           url: urlLink,
           method: "GET"
           }).done(function(response) {
 
-            console.log(response);
           //For loop to render all the images till the end of length of API array
           for(var i=0; i < response.data.length; i++){
 
-          //Image is captured in the imageURL from the response
+          //Animated Image is captured in the imageAnimated from the response
           var imageAnimated = response.data[i].images.fixed_height.url;
 
+          //Still Image is captured in the imageStill from the response
           var imageStill = response.data[i].images.fixed_height_still.url;
 
-          //creating the div
+          //Creating the div with the class animalClass
           var animalDiv = $("<div class = 'animalClass'>");
 
           //AnimalImg is equated to the IMG tag which is being created using jQuery
           var animalImg = $("<img>");
 
-          //Give the image tag through animalImg the attributes of the URL source of the cat image
-          animalImg.attr("src", imageStill);
-          animalImg.attr("alt", "animal image");
-          animalImg.attr("data-still", imageStill);
-          animalImg.attr("data-animate", imageAnimated);
-          animalImg.attr("data-state", "still");
-          animalImg.attr("class", "gif");
+          //Adding attributes the the animalImg 
+          animalImg.attr({
+            "src": imageStill,
+            "alt": "animal image",
+            "data-still": imageStill,
+            "data-animate": imageAnimated,
+            "data-state": "still",
+            "class": "gif"
+          });
 
+          //animalImg is appended into animalDiv
           animalDiv.append(animalImg);
 
-          //Rendering the Rating of the GIF
+          //Rendering the Rating of the GIF using a p tag
           $("<p>Rating: " + response.data[i].rating  + "</p>").appendTo(animalDiv);
 
-          //Captured animal image is rendered in the screen into the images div using the prepend function
+          //Captured animal image is rendered in the screen into the results div using the appepend function
           $("#results").append(animalDiv);
         }    
     }); 
@@ -100,23 +98,26 @@ function getGifInfo(urlLink){
 // This function handles events where one button is clicked
 $("#addanimal").on("click", function(event) {
 
+        //Prevents the deault part to occur
         event.preventDefault();
 
         // This line grabs the input from the textbox
         var animal = $("#animal-input").val().trim();
 
+        //checking the condition if user enters nothing then button should not be created
         if(animal === ""){
 
-          alert("Unable to render button with the empty name");
+          alert("Unable To Render Button With The Empty Name");
   
         }
 
+        //Control goes here if user enters something in the textbox
         else{
 
         // The movie from the textbox is then added to our array
         topics.push(animal);
 
-        // Calling renderButtons which handles the processing of our movie array
+        // Calling renderButtons which handles the processing of rendering the new set of buttons
         renderButtons();
 
         // Clearing out the Textbox after the Button has been rendered on screen
@@ -124,9 +125,10 @@ $("#addanimal").on("click", function(event) {
       }
       });
 
+//This funtion gets called when user clicks the image to change its state
 $(document).on("click", ".gif", function() {
       
-    console.log("Click is working");    
+    //variable state is used to grab the surrent state of the clicked image    
     var state = $(this).attr("data-state");
      
     //Check if the variable state is equal to 'still',
@@ -134,10 +136,11 @@ $(document).on("click", ".gif", function() {
     // and update the data-state attribute to 'animate'.
 
     if(state === "still"){
-      var animate = $(this).attr("data-animate"); //captures the data-animate URL into variable animate
-      $(this).attr("src", animate); //updates the src to what we have in the varibale animate
-      $(this).attr("data-state", "animate"); //updates the data-state to the word "animate"
-      
+      var animate = $(this).attr("data-animate"); 
+      $(this).attr({
+        "src": animate, 
+        "data-state": "animate"
+      }); 
     }
 
     // If state is equal to 'animate', then update the src attribute of this
@@ -145,8 +148,10 @@ $(document).on("click", ".gif", function() {
     
     else if(state === "animate"){
         var still = $(this).attr("data-still"); //captures the data-still URL into variable still
-        $(this).attr("src", still); //updates the src to what we have in the varibale still
-        $(this).attr("data-state", "still"); //updates the data-state to the word "still"
+        $(this).attr({
+          "src": still, 
+          "data-state": "still"
+          }); 
     }
 });
 
